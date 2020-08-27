d24ba53dd423e504c8d93a627e8025aa9e3d52e5
A601-22 <tnk.dat.class@gmail.com>
2020-08-27 11:12:42 +0900

    演習11-1

--- v0827-1/Form1.cs
+++ v0827-1/Form1.cs
@@ -12,9 +12,40 @@ namespace v0827_1
 {
     public partial class Form1 : Form
     {
+        int vx = -3;
+        int vy = -3;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point mp = MousePosition;
+            mp = PointToClient(mp);
+            label1.Left += vx;
+            label1.Top += vy;
+            label2.Text = "" + mp.X + "," + mp.Y;
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+            if ((label1.Left <= mp.X) && (label1.Right > mp.X) && (label1.Top <= mp.Y) && (label1.Bottom > mp.Y))
+            {
+                timer1.Enabled=false;
+            }
         }
     }
 }
