46025a53a0a611222ecaf67bd96afa6bcbb1e381
A601-22 <tnk.dat.class@gmail.com>
2020-08-27 12:22:20 +0900

    演習13-1

--- v0827-1/Form1.Designer.cs
+++ v0827-1/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label3 = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -45,6 +47,7 @@
             this.label1.Size = new System.Drawing.Size(67, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -77,11 +80,33 @@
             this.timer2.Interval = 10;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.label4.Location = new System.Drawing.Point(355, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.label5.Location = new System.Drawing.Point(464, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(780, 598);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -99,6 +124,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- v0827-1/Form1.cs
+++ v0827-1/Form1.cs
@@ -15,6 +15,10 @@ namespace v0827_1
         static Random rand = new Random();
         int vx = rand.Next(-7, 8);
         int vy = rand.Next(-7, 8);
+        int vx1 = rand.Next(-7, 8);
+        int vy1 = rand.Next(-7, 8);
+        int vx2 = rand.Next(-7, 8);
+        int vy2 = rand.Next(-7, 8);
         int itime = 0;
         public Form1()
         {
@@ -22,6 +26,10 @@ namespace v0827_1
             
             label1.Left = rand.Next(ClientSize.Width-label1.Width);
             label1.Top = rand.Next(ClientSize.Height-label1.Height);
+            label4.Left = rand.Next(ClientSize.Width - label1.Width);
+            label4.Top = rand.Next(ClientSize.Height - label1.Height);
+            label5.Left = rand.Next(ClientSize.Width - label1.Width);
+            label5.Top = rand.Next(ClientSize.Height - label1.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -30,6 +38,10 @@ namespace v0827_1
             mp = PointToClient(mp);
             label1.Left += vx;
             label1.Top += vy;
+            label4.Left += vx1;
+            label4.Top += vy1;
+            label5.Left += vx2;
+            label5.Top += vy2;
             label2.Text = "" + mp.X + "," + mp.Y;
             itime += 1;
             label3.Text = "score:" + itime;
@@ -49,6 +61,40 @@ namespace v0827_1
             {
                 vy = -Math.Abs(vy);
             }
+
+            if (label4.Left < 0)
+            {
+                vx1 = Math.Abs(vx1);
+            }
+            if (label4.Top < 0)
+            {
+                vy1 = Math.Abs(vy1);
+            }
+            if (label4.Right > ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vx1);
+            }
+            if (label4.Bottom > ClientSize.Height)
+            {
+                vy1 = -Math.Abs(vy1);
+            }
+
+            if (label5.Left < 0)
+            {
+                vx2 = Math.Abs(vx2);
+            }
+            if (label5.Top < 0)
+            {
+                vy2 = Math.Abs(vy2);
+            }
+            if (label5.Right > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx2);
+            }
+            if (label5.Bottom > ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy2);
+            }
             if ((label1.Left <= mp.X) && (label1.Right > mp.X) && (label1.Top <= mp.Y) && (label1.Bottom > mp.Y))
             {
                 timer1.Enabled=false;
@@ -61,6 +107,11 @@ namespace v0827_1
             label3.Top = ClientSize.Height - label3.Height;
             label2.Left = 0;
             label2.Top = ClientSize.Height - label2.Height - label3.Height;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
